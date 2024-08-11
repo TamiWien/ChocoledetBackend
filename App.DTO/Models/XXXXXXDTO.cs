647a813f1fe2129c3d183ad31249c7394ba46fdb
Tami <tamivnet@gmail.com>
2024-08-11 17:54:14 +0300

    Add XXXXX Model

---
+++ App.DTO/Models/XXXXXXDTO.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.DTO.Models
+{
+    public class CategoryDTO
+    {
+        public int XXXXXXXXXXXXXXX { get; set; }
+
+        public string VVVVVVVVVVVVVVVVVVV { get; set; } = null!;
+
+    }
+}
